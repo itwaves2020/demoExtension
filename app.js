42f3cdceb8addfdaff01a122fd98e3d6d4671fe3
Manish <manish24898@gmail.com>
2021-04-23 14:54:09 +0530

    All

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+app.set('trust proxy', 1);
+
+const MemoryStore = session.MemoryStore;
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    store: new MemoryStore(),
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.listen(8000, () => {
+    console.log("Server is running....");
+});
+
+app.use(express.static("./bundle"));
+
+app.get("/test", (req, res) => {
+    console.log(req.session.cookie);
+    res.cookie('cookieName', 123, { maxAge: 900000, httpOnly: true });
+    res.status(200).send({ data: { message: "success" } });
+});
